b45742a4d779e53ca6875c6a5ecf4af518e85cce
Brett Lyons <blyons@students.naropa.edu>
2016-05-08 19:20:00 -0600

    1 test in coins change passing

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -43,9 +43,23 @@ object Main {
   /**
    * Exercise 3
    */
+    // so for each quantity in coins,
+    // find both the maximum possible amount that can be made with that denomination
+    // and the minimum possible amount that can be made with that denomination
+    // and then use that to run innerCountChange w/ the new money amount +
+    // the remaining coins list +
+    // the the accumulated quantity
   def countChange(money: Int, coins: List[Int]): Int = {
-
-
+    def innerCountChange(money: Int, coins: List[Int], accumulatedPossibleWays: Int): Int = {
+      if(money == 0 || coins.isEmpty)
+        accumulatedPossibleWays
+      else
+        // recur
+        if (money % coins.head != 0)
+          accumulatedPossibleWays
+        else
+          innerCountChange(money - coins.head, coins.tail, accumulatedPossibleWays + 1)
+    }
+    innerCountChange(money, coins, 0)
   }
-
 }
