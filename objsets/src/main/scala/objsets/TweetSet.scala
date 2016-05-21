d658c67306d1f3d557e7e79b3a4ea12624762f40
Brett Lyons <blyons@students.naropa.edu>
2016-05-21 17:30:00 -0600

    Re-started from nothing.  Partial impl. of filterAcc.

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -41,19 +41,12 @@ abstract class TweetSet {
    * Question: Can we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-  def filter(p: Tweet => Boolean): TweetSet = {
-    filterAcc(p, new NonEmpty(this, new Empty, new Empty))
-  }
+    def filter(p: Tweet => Boolean): TweetSet = ???
   
   /**
    * This is a helper method for `filter` that propagetes the accumulated tweets.
    */
   def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet
-  // {
-  //   if(p(_)) acc.incl(_)
-  //   else filterAcc(p, acc) 
-  // }
-
 
   /**
    * Returns a new `TweetSet` that is the union of `TweetSet`s `this` and `that`.
@@ -114,8 +107,7 @@ abstract class TweetSet {
 }
 
 class Empty extends TweetSet {
-  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = new Empty
-  // new NonEmpty(acc, new Empty, new Empty)
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = new Empty
   
   /**
    * The following methods are already implemented
@@ -133,9 +125,10 @@ class Empty extends TweetSet {
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
   def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = {
-    if(p(elem)) acc.incl(elem)
-    else filterAcc
+    if(p(elem)) filterAcc(p, acc.incl(elem))
+    else filterAcc(p, acc)
   }
+  
     
   /**
    * The following methods are already implemented
